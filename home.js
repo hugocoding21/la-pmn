cb39bfaba15401531327da23a5ebfd1ec3f162c1
madou936 <madou936@outlook.fr>
2023-10-26 12:24:30 +0200

    responsive

--- home.js
+++ home.js
@@ -12,3 +12,15 @@ function changeSlide(direction) {
 }
 
 showSlide(0);
+
+
+const button = document.getElementById("my-button");
+const nav = document.getElementsByClassName("part1");
+
+button.addEventListener('click', () => {
+  nav.classList.toggle('show');
+});
+
+button.addEventListener('click', function() {
+  document.body.style.backgroundColor = 'red';
+}
